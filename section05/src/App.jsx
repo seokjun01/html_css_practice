8216ad9d5baeb864e4813cc2c8a6688fb0df9411
seokjun01 <suckjungod@naver.com>
2026-04-04 13:33:29 +0900

    React - miniProjec UI구현

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import Header from "./components/Header";
+import List from "./components/List";
+import Editor from "./components/Editor";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;
