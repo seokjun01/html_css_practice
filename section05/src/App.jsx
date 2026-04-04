2ed60b4ca8e6f0d578b41e6989b43f9a4c579f39
seokjun01 <suckjungod@naver.com>
2026-04-04 13:55:51 +0900

    React - 디테일추가 (엔터시 제출 , 제출시 입력칸 초기화)

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
@@ -27,10 +27,23 @@ const mockData = [
 
 function App() {
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(0);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: 0,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos({ newTodo, ...todos });
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- section05/src/components/Editor.jsx
+++ section05/src/components/Editor.jsx
@@ -1,10 +1,39 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContnet = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onsubmit();
+    }
+  };
+
+  const onsubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo .." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onChange={onChangeContnet}
+        onKeyDown={onKeydown}
+        placeholder="새로운 Todo .."
+      />
+      <button onClick={onsubmit}>추가</button>
     </div>
   );
 };
