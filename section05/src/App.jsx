9b5805eccc66f9713ca321104d336972272071eb
seokjun01 <suckjungod@naver.com>
2026-04-04 13:40:18 +0900

    add : 상태선언 및 MockData 추가

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
 import "./App.css";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
   return (
     <div className="App">
       <Header />
