8ab5e606f79bcde9d75f4ae7ab82a8359b3d3a3c
seokjun01 <suckjungod@naver.com>
2026-04-05 17:12:05 +0900

    UseMemo사용

--- section05/src/components/List.jsx
+++ section05/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -18,9 +18,27 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  //deps에 따라 ..
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("호출");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
+  //   const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
   return (
     <div className="List">
       <h4>Todo List🌱</h4>
+      <div>total : {totalCount}</div>
+      <div>done : {doneCount}</div>
+      <div>notDone : {notDoneCount}</div>
       <input
         value={search}
         onChange={onChangeSearch}
