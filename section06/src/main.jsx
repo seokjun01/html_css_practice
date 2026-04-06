ec726bc899e68b7748e19388a0aaacaca291f3bd
seokjun01 <suckjungod@naver.com>
2026-04-06 21:58:42 +0900

    chore : 라우팅 세팅

--- section06/src/main.jsx
+++ section06/src/main.jsx
@@ -1,4 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
