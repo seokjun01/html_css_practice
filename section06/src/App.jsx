33e98dda78f06acf7cdf0105797fcc488f80b514
seokjun01 <suckjungod@naver.com>
2026-04-07 22:28:07 +0900

    feat : 공통컴퍼넌트 구현

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -5,7 +5,8 @@ import New from "./pages/New";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Notfound from "./pages/Notfound";
 import { getEmotionImage } from "./util/get-emotion-image";
-
+import Button from "./components/Button";
+import Header from "./components/Header";
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 //2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
@@ -18,18 +19,33 @@ function App() {
   };
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+      <Button
+        text={"123"}
+        type={"DEFAULT"}
+        onClick={() => {
+          console.log("123번 버튼");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼");
+        }}
+      />
+
       <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
         <Route path="/" element={<Home />} />
