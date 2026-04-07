a6f81687d00c876173e81c924055df5c7c259a1b
seokjun01 <suckjungod@naver.com>
2026-04-07 22:00:08 +0900

    이미지 개수가 적다면 assets에 캐싱

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -4,6 +4,11 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Notfound from "./pages/Notfound";
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 //2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -18,6 +23,13 @@ function App() {
   return (
     <>
       <div>
+        <img src={emotion1} />
+        <img src={emotion2} />
+        <img src={emotion3} />
+        <img src={emotion4} />
+        <img src={emotion5} />
+      </div>
+      <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
@@ -26,7 +38,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/Diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
